c67a0183acb394d21020542f70e63d05b05e3ceb
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-09-19 01:17:17 +0900

    Build_ToDoList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
+import ToDoHead from "./components/ToDoHead";
+import ToDoList from "./components/ToDoList";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <ToDoHead />
+        <ToDoList />
+      </TodoTemplate>
+    </>
   );
 }
 
